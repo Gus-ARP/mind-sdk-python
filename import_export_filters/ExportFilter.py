4398bb57e47e11a138ddd62b002276fd9a05f5b4
Almerxsese <echopraxium@yahoo.com>
2015-11-24 11:21:16 +0100

    Create ExportFilter.py

---
+++ import_export_filters/ExportFilter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""
+    xmind.import_export_filters.ExportFilter
+    ~~~~~~~~~~~
+    :mod:``xmind.import_export_filters.ExportFilter`` provide a handy way for exporting / importing 
+    XMind files to other formats (e.g: GraphViz).
+    :copyright:
+    :license: MIT
+"""
+
+__author__ = "echopraxium@yahoo.com <Michel Kern>"
+
+import os
+import os.path
+
+FILE_NOT_FOUND_ERROR = 1
+
+class ExportFilter:
+    def __init__(self):
+        self.name = "export_filter"
+    
+    #---------- export() ----------    
+    def export(self, source_path, target_path):
+        print("export_filter.name: '" + self.name + "'")
+        if (not os.path.isfile(source_path)): 
+            print("source_path: '" + source_path + "' not found")
+            exit(FILE_NOT_FOUND_ERROR)
+    #---------- export()
+
+#======================== main ========================
+def main():
+    print("** xmind.ExportFilter **")
+    export_filter = ExportFilter()
+    export_filter.export("", "")
+    #pass
+
+if __name__ == '__main__':
+    main()
