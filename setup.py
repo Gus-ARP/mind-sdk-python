c141d3997266e1905e426959180937653fdef442
Almerxsese <echopraxium@yahoo.com>
2015-11-30 22:54:30 +0100

    15

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="xmind",
-    version="0.1a.0",
+    version="0.0.1",
     packages=find_packages(),
 
     install_requires=["distribute"],
 
-    author="Woody Ai",
-    author_email="aiqi@xmind.net",
-    description="The offical XMind python SDK",
+    author="Michel Kern",
+    author_email="echopraxium@yahoo.com",
+    description="A fork of XMind python SDK",
     license="MIT",
     keywords="XMind, SDK, mind mapping",
-    url="https://github.com/xmindltd/xmind-sdk-python"
+    url="https://github.com/Almerxsese/xmind-sdk-python"
 )
