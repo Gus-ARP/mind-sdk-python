487a34ca6cf2aa407bde3bc5af81f0e90ba0631d
Almerxsese <echopraxium@yahoo.com>
2015-11-30 22:36:37 +0100

    12

--- xmind/import_export/__init__.py
+++ xmind/import_export/__init__.py
@@ -2,16 +2,16 @@
 #-*- coding: utf-8 -*-
 """
     xmind.import_export
-    ~~~~~~~~~~
+    -------------------
     :copyright: Michel Kern
-    :license: MIT
+    :license:   MIT
 
 """
 __author__ = "echopraxium@yahoo.com <Michel Kern>"
 
 #======================== main ========================
 def main():
-    print("** xmind.__init__ **")
+    print("** xmind.import_export module **")
 
 if __name__ == '__main__':
     main()
